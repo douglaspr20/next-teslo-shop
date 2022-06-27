f5433d64f4ca1a8e1053b4f2feace4310385d3af
Douglas Perez <douglas.eduardo2000@gmail.com>
2022-06-26 22:38:21 -0400

    update

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -3,14 +3,6 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { jwt } from "../../utils";
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  // const { token = "" } = req.cookies;
-  // try {
-  //   await jwt.isValidToken(token);
-  //   return NextResponse.next();
-  // } catch (error) {
-  //   return NextResponse.redirect(`auth/logun?p=${req.page.name}`);
-  // }
-
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!session) {
